481b261940ef58be5163037be95dfd1188f0550c
email95 <qq1255330641>
2018-07-15 19:38:21 +0800

    异常捕获

--- src/main/java/com/chen/sell/handler/SellerExceptionHandler.java
+++ src/main/java/com/chen/sell/handler/SellerExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.chen.sell.handler;
 
 
+import com.chen.sell.VO.ResultVO;
 import com.chen.sell.config.ProjectConfigUrl;
+import com.chen.sell.exception.SellException;
+import com.chen.sell.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -22,5 +27,11 @@ public class SellerExceptionHandler {
                 .concat("?returnUrl=")
                 .concat(projectConfigUrl.getSell()
                 .concat("/sell/seller/login")));
+    }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    public ResultVO handlerSellerException(SellException e){
+        return ResultVOUtil.error(e.getCode(),e.getMessage());
     }
 }
